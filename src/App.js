a0a4439776803869b171b19628c052c49a1e04ed
haweecodes <haweesajib@yahoo.com>
2019-05-26 11:50:10 +0600

    app complete for use
    added the datas for viewing
    added geolocation
    added timer for reloading

--- src/App.js
+++ src/App.js
@@ -1,82 +1,213 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios'
-import config from './config.js'
-
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import config from "./config.js";
+import { WiDayHaze, WiCloud, WiDaySunny, WiDayRain } from "react-icons/wi";
+var cities = require("./bd-city.json");
 class App extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        value: "",
-        weatherData: "",
-        isError: false
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSearch = this.handleSearch.bind(this);
+    this.timer = null
+    this.state = {
+      weatherData: [],
+      isError: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.reset= this.reset.bind(this);
   }
 
   // save the search value onChange
   handleChange(e) {
-    this.setState({ value: e.target.value }, () => {});    
-  }
-
-  // function to handle api call of the searched city
-  handleSearch = event => {
-    let response =" "
-     if (event.key === 'Enter') {
+    if (e.target.value !== 0) {
+      this.setState(
+        {
+          isError: false
+        },
+        () => {}
+      );
+      let response = " ";
       response = new Promise((resolve, reject) => {
-       axios.get(config+ "current?city="+ this.state.value +"&country=BD&key=cc41ae1d1c974f3cb029aff199d62a35")
-      .then(res => {
-        resolve(res.data.data[0])
-        }).catch(err => {
-          reject(err.response)
-        })
+        axios
+          .get(
+            config +
+              "current?city=" +
+              e.target.value +
+              "&country=BD&key=cc41ae1d1c974f3cb029aff199d62a35"
+          )
+          .then(res => {
+            if (res.status !== 204) {
+              resolve(res.data.data);
+            } else {
+              reject(res.statusText);
+            }
+          })
+          .catch(err => {
+            reject(err.response);
+          });
       });
 
-      response.then(response => {
-        console.log(response);
-        this.setState({
-          weatherData: response
-        })
-      }).catch(err => {
-        this.setState({
-          isError: true
-        })
-      })
-
-     }
-  }
-  render(){
-    return (
-        <div className="App">
-          <main>
-            {/* input to search for the a city */}
-            <input type="text" id="city" name="search_box" placeholder="Type a city name" onKeyDown={this.handleSearch}  onChange={this.handleChange}/>
-
-            {/* display box to show the weather condition of the searched city */}
+      response
+        .then(response => {
+          this.setState(
             {
-              this.state.isError != true ?
-                 <div className="card">
-                  <div className="card--body">
-                  <p>City Name: {this.state.weatherData.city_name || "N/A"}</p>
-                  <p>Temperature: {this.state.weatherData.temp || 0} °C</p>
-                  <p>Time Zone: {this.state.weatherData.timezone || "N/A"}</p>
-                  {/* <p>Description: {this.state.weatherData.weather || "N/A"} </p> */}
-                  </div>
-                </div>
-                :
-                <div>
-                  <p>Sorry that city is not listed in our system</p>
-                </div>
-              
-            }
-           
+              weatherData: response
+            },
+            () => {}
+          );
+        })
+        .catch(err => {
+          this.setState(
+            {
+              isError: true
+            },
+            () => {}
+          );
+        });
+    }
+  }
 
-          </main>
+  componentDidMount() {
+    let response = " ";
+    //get the current location to see the current places data
+
+    navigator.geolocation.getCurrentPosition(position => {
+      response = new Promise((resolve, reject) => {
+        axios
+          .get(
+            config +
+              "current?lat=" +
+              position.coords.latitude +
+              "&lon=" +
+              position.coords.longitude +
+              "&key=cc41ae1d1c974f3cb029aff199d62a35"
+          )
+          .then(res => {
+            if (res.status !== 204) {
+              resolve(res.data.data);
+            } else {
+              reject(res.statusText);
+            }
+          })
+          .catch(err => {
+            reject(err.response);
+          });
+      });
+      response
+        .then(response => {
+          this.setState(
+            {
+              weatherData: response
+            },
+            () => {}
+          );
+        })
+        .catch(err => {
+          this.setState(
+            {
+              isError: true
+            },
+            () => {}
+          );
+        });
+    });
+
+    // events to check for reseting the timer
+
+    document.addEventListener("mousemove", this.reset, false);
+    document.addEventListener("mousedown", this.reset, false);
+    document.addEventListener("keypress", this.reset, false);
+    document.addEventListener("touchmove", this.reset, false);
+
+    // set the timer, reset timer 5 min
+    this.timer = setTimeout(() => {
+      window.location.reload();
+    }, 300000);
+   
+  }
+
+  // reseting function for the app
+  reset() {
+    clearTimeout(this.timer);
+    this.timer = null
+    this.timer = setTimeout(() => {
+      window.location.reload();
+    }, 300000);
+  }
+
+  render() {
+    let cityArray = [];
+    let cardContent = [];
+
+    cityArray.push(
+      <option value="0" key="0">
+        Select A City
+      </option>
+    );
+    cities.districts.forEach((element, i) => {
+      cityArray.push(
+        <option value={element.name} key={i + 1}>
+          {element.name}
+        </option>
+      );
+    });
+
+    //show error if the data is not found
+    if (this.state.isError) {
+      cardContent.push(
+        <div className="card" key="1">
+          <div className="card--body">
+            {" "}
+            <p>Sorry the database has no info about this city</p>
+          </div>
         </div>
       );
+    } else {
+      let cloud = "";
+      this.state.weatherData.map((user, i) => {
+        // a switch statement for selecting an icon depending on the weather
+        switch (user.weather.description) {
+          case "Sunny":
+            cloud = <WiDaySunny className="card-body__icon" />;
+            break;
+          case "Rain":
+            cloud = <WiDayRain className="card-body__icon" />;
+            break;
+          case "Haze":
+            cloud = <WiDayHaze className="card-body__icon" />;
+            break;
+          default:
+            cloud = <WiCloud className="card-body__icon" />;
+            break;
+        }
+
+        // push the card with the datas of the weather
+        cardContent.push(
+          <div className="card" key={i}>
+            <div className="card--body">
+              {cloud}
+              <p className="card-body__temp">{user.temp || 0} °C </p>
+              <p>Time Zone: {user.timezone || "N/A"}</p>
+              <p>City Name: {user.city_name || "N/A"}</p>
+              <p>Description: {user.weather.description} </p>
+            </div>
+          </div>
+        );
+      });
     }
-  
+    return (
+      <div className="App">
+        <main>
+          {/* input to search for the a city */}
+          <select name="dropdown" onChange={this.handleChange}>
+            {cityArray}
+          </select>
+
+          {/* display box to show the weather condition of the searched city */}
+          {cardContent}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
